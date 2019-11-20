d0e624c93f886e0733cbde58845eb7e589eecaff
Derek Provance <derek@provance.me>
2019-11-20 17:19:08 -0600

    Calendar prep

--- app/src/main/java/com/derekprovance/biometricsapp/MainActivity.java
+++ app/src/main/java/com/derekprovance/biometricsapp/MainActivity.java
@@ -41,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fab = findViewById(R.id.calendarAction);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
