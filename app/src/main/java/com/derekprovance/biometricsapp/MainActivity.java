4ef9ba7d68c79930c4236c2ec27d63c7f06bb247
Derek Provance <derek@provance.me>
2019-11-20 10:56:26 -0600

    Blood Sugar Placeholder

--- app/src/main/java/com/derekprovance/biometricsapp/MainActivity.java
+++ app/src/main/java/com/derekprovance/biometricsapp/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_blood_sugar, R.id.nav_meal_logging, R.id.nav_water_consumption)
+                R.id.nav_home, R.id.nav_blood_sugar)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
